6774e87fb59b5036272811be8098d531bba8a83f
Timi Voutilainen <timi_v@hotmail.com>
2018-12-28 13:41:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -420,7 +420,7 @@ exports.Document = function() {
 			{
 				var relsXml = this._utf8ArrayToString(zip.file("word/_rels/document.xml.rels")._data.getContent());
 				relsXmlBuilder.push('</Relationships>');
-				relsXml = relsXml.replace('</Relationships>', relsXmlBuilder.join(''));
+              relsXml = relsXml.replace('</Relationships>', relsXmlBuilder.join(''));
 				zip.file("word/_rels/document.xml.rels", relsXml);
 			}
 			
@@ -431,8 +431,7 @@ exports.Document = function() {
 		}
 
       var addBody = this._utf8ArrayToString(zip.file("word/document.xml")._data.getContent());
-      var watermark = `<w:sdtContent><w:r><w:pict w14:anchorId="15987AD4"><v:shapetype id="_x0000_t136" coordsize="21600,21600" o:spt="136" adj="10800" path="m@7,l@8,m@5,21600l@6,21600e"><v:formulas><v:f eqn="sum #0 0 10800"/><v:f eqn="prod #0 2 1"/><v:f eqn="sum 21600 0 @1"/><v:f eqn="sum 0 0 @2"/><v:f eqn="sum 21600 0 @3"/><v:f eqn="if @0 @3 0"/><v:f eqn="if @0 21600 @1"/><v:f eqn="if @0 0 @2"/><v:f eqn="if @0 @4 21600"/><v:f eqn="mid @5 @6"/><v:f eqn="mid @8 @5"/><v:f eqn="mid @7 @8"/><v:f eqn="mid @6 @7"/><v:f eqn="sum @6 0 @5"/></v:formulas><v:path textpathok="t" o:connecttype="custom" o:connectlocs="@9,0;@10,10800;@11,21600;@12,10800" o:connectangles="270,180,90,0"/><v:textpath on="t" fitshape="t"/><v:handles><v:h position="#0,bottomRight" xrange="6629,14971"/></v:handles><o:lock v:ext="edit" text="t" shapetype="t"/></v:shapetype><v:shape id="PowerPlusWaterMarkObject357831064" o:spid="_x0000_s2049" type="#_x0000_t136" style="position:absolute;left:0;text-align:left;margin-left:0;margin-top:0;width:412.4pt;height:247.45pt;rotation:315;z-index:-251656704;mso-position-horizontal:center;mso-position-horizontal-relative:margin;mso-position-vertical:center;mso-position-vertical-relative:margin" o:allowincell="f" fillcolor="silver" stroked="f"><v:fill opacity=".5"/><v:textpath style="font-family:&quot;calibri&quot;;font-size:1pt" string="LUONNOS"/><w10:wrap anchorx="margin" anchory="margin"/></v:shape></w:pict></w:r></w:sdtContent>`;
-
+      var watermark = `<w:sdt><w:sdtPr><w:id w:val="-1960019101"/><w:docPartObj><w:docPartGallery w:val="Watermarks"/><w:docPartUnique/></w:docPartObj></w:sdtPr><w:sdtContent><w:r><w:pict w14:anchorId="59CEE41C"><v:shapetype id="_x0000_t136" coordsize="21600,21600" o:spt="136" adj="10800" path="m@7,l@8,m@5,21600l@6,21600e"><v:formulas><v:f eqn="sum #0 0 10800"/><v:f eqn="prod #0 2 1"/><v:f eqn="sum 21600 0 @1"/><v:f eqn="sum 0 0 @2"/><v:f eqn="sum 21600 0 @3"/><v:f eqn="if @0 @3 0"/><v:f eqn="if @0 21600 @1"/><v:f eqn="if @0 0 @2"/><v:f eqn="if @0 @4 21600"/><v:f eqn="mid @5 @6"/><v:f eqn="mid @8 @5"/><v:f eqn="mid @7 @8"/><v:f eqn="mid @6 @7"/><v:f eqn="sum @6 0 @5"/></v:formulas><v:path textpathok="t" o:connecttype="custom" o:connectlocs="@9,0;@10,10800;@11,21600;@12,10800" o:connectangles="270,180,90,0"/><v:textpath on="t" fitshape="t"/><v:handles><v:h position="#0,bottomRight" xrange="6629,14971"/></v:handles><o:lock v:ext="edit" text="t" shapetype="t"/></v:shapetype><v:shape id="PowerPlusWaterMarkObject357831064" o:spid="_x0000_s2049" type="#_x0000_t136" style="position:absolute;left:0;text-align:left;margin-left:0;margin-top:0;width:412.4pt;height:247.45pt;rotation:315;z-index:-251656704;mso-position-horizontal:center;mso-position-horizontal-relative:margin;mso-position-vertical:center;mso-position-vertical-relative:margin" o:allowincell="f" fillcolor="silver" stroked="f"><v:fill opacity=".5"/><v:textpath style="font-family:&quot;calibri&quot;;font-size:1pt" string="LUONNOS"/><w10:wrap anchorx="margin" anchory="margin"/></v:shape></w:pict></w:r></w:sdtContent></w:sdt>`;
       var body = "";
 
       String.prototype.splice = function (idx, rem, str) {
@@ -441,9 +440,8 @@ exports.Document = function() {
 
       if (isDraft && zip.file("word/header3.xml") != null) {
         var addHeader = this._utf8ArrayToString(zip.file("word/header3.xml")._data.getContent());
-         var newHeader = addHeader.splice(addHeader.indexOf(`</w:sdtPr>`), 0, watermark);
+         var newHeader = addHeader.splice(addHeader.indexOf(`</w:pPr>`) + 8, 0, watermark);
         zip.file("word/header3.xml", newHeader);
-        body += `<w:footerReference w:type="default" r:id="rId15"/><w:headerReference w:type="first" r:id="rId16"/><w:footerReference w:type="first" r:id="rId17"/>`;
       }
 
       body += `<w:p w:rsidR="005F670F" w:rsidRDefault="005F79F5"><w:r><w:t>{@body}</w:t></w:r></w:p>`;
